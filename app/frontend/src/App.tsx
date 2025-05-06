2fec09aaafe128adc493f4be1498c901e99e5ffa
Nickolausen <nicholas.magi24@gmail.com>
2025-05-06 16:59:14 +0200

    init frontend app

---
+++ app/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <>
+      <Button variant={"secondary"}>Click me!</Button>      
+    </>
+  )
+}
+
+export default App
