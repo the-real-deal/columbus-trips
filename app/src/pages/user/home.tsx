8da244e4abe4ebdda2a94922a899b015132b9222
Nicholas Magi <nicholas.magi24@gmail.com>
2025-06-16 10:15:34 +0200

    interesting activities

--- app/src/pages/user/home.tsx
+++ app/src/pages/user/home.tsx
@@ -1,10 +1,10 @@
 import DefaultLayout from "@/components/layout/default-layout";
 import PopularDestinations from "@/components/popular-destinations/popular-destionations";
-import PopularTrips from "@/components/popular-trips/popular-trips";
+import InterestingActivities from "@/components/interesting-activities";
 
 export default function Home() {
     return <DefaultLayout className="py-0">
         <PopularDestinations className="pt-5"/>
-        <PopularTrips className="py-10"/>
+        <InterestingActivities username="PaoloBitta77" className="py-10"/>
     </DefaultLayout>
 }
